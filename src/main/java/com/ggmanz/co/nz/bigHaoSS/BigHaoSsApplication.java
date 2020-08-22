8f6250a226c7075d5c1ae3eb96aefa4f3e8403ba
Michael <michael2110@sina.com>
2020-08-23 11:01:39 +1200

    initial project generated by springBoot

---
+++ src/main/java/com/ggmanz/co/nz/bigHaoSS/BigHaoSsApplication.java
@@ -0,0 +1,13 @@
+package com.ggmanz.co.nz.bigHaoSS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigHaoSsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigHaoSsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ggmanz/co/nz/bigHaoSS/BigHaoSsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggmanz.co.nz.bigHaoSS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigHaoSsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
