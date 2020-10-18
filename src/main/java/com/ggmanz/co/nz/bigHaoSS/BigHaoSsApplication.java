a3c9ecae205c05582fd9f2b3c224f90869d14c0b
Michael <michael2110@sina.com>
2020-10-19 12:12:49 +1300

    modify init function

--- src/main/java/com/ggmanz/co/nz/bigHaoSS/BigHaoSsApplication.java
+++ src/main/java/com/ggmanz/co/nz/bigHaoSS/BigHaoSsApplication.java
@@ -3,11 +3,17 @@ package com.ggmanz.co.nz.bigHaoSS;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @SpringBootApplication
 public class BigHaoSsApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(BigHaoSsApplication.class, args);
+
 	}
 
 }
